bfbda5c71ee2f8ade205d5b72abcc1c90a74d0ca
AnuragGarimella <anurag.garimella@mitratech.com>
2020-08-03 01:23:38 +0530

    Removed unnecesary using statements in ChatHub and added the Startup Class.

---
+++ SignalRChatExperiment/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChatExperiment.Startup))]
+namespace SignalRChatExperiment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any Hub/Conection wire up & configuration should go in here.
+            app.MapSignalR();
+        }
+    }
+}
